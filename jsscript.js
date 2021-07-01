29c4f8129111fe8782d5aad296fe17fd330ba64e
ArtemSonin21 <aclens@yandex.ru>
2021-07-01 20:46:04 +0300

    Оживляем интерактивные элементы 2
    Добавлены элементы для анимации но почему то она не работает...

--- jsscript.js
+++ jsscript.js
@@ -2,12 +2,23 @@ console.log("какой-то текст");
 
 const button = document.querySelector(".button-serch");
 const popup = document.querySelector(".form-serch");
-const form = popup.querySelector("form-wrap");
+const form = popup.querySelector(".form-wrap");
+const checkInDate = popup.querySelector("[name=check-in]");
+const checkOutDate = popup.querySelector("[name=check-out]");
 
 button.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	popup.classList.toggle("visually-hidden");
 	console.log("клик по кнопке убрать окно");
+});
+
+form.addEventListener("submit", function () {
+	if (!checkInDate.value || !checkOutDate.value) {
+		
+		popup.classList.add("modal-error");
+		console.log("нужно ввести даты поездки");
+
+	}
 });
 
 
